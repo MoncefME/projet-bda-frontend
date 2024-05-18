01917243ee22c69a04154409b8db1344e7b8382d
HabibaMoulfi <kh_moulfi@esi.dz>
2024-05-18 13:13:06 +0100

    Merging with the main

--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -29,7 +29,7 @@ const Navbar = () => {
       </div>
       <div className="flex items-center space-x-2">
       <button onClick={openModal} className="bg-[#fc4c02] text-white px-4 py-2 rounded">
-            Create New Run
+            Add New Run
           </button>
 
         <div className="flex flex-col items-end ">
